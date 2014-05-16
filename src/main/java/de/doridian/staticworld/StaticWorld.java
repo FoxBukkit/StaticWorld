dd56ef181bc5acfca72936ef0a6bc230f692c609
Doridian Draconia <mriq91@gmail.com>
2014-05-17 00:26:52 +0200

    Block a few more events, reformat code

--- src/main/java/de/doridian/staticworld/StaticWorld.java
+++ src/main/java/de/doridian/staticworld/StaticWorld.java
@@ -7,65 +7,35 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StaticWorld extends JavaPlugin implements Listener {
-	@Override
-	public void onEnable() {
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-	}
+	@Override public void onEnable() { Bukkit.getServer().getPluginManager().registerEvents(this, this); }
 
-	@EventHandler
-	public void onPhysics(BlockPhysicsEvent event) {
-		event.setCancelled(true);
-	}
+	@EventHandler public void onPhysics(BlockPhysicsEvent event) { event.setCancelled(true); }
+	@EventHandler public void onLeavesDecay(LeavesDecayEvent event) { event.setCancelled(true); }
+	@EventHandler public void onBlockFade(BlockFadeEvent event) { event.setCancelled(true); }
+	@EventHandler public void onBlockGrow(BlockGrowEvent event) { event.setCancelled(true); }
+	@EventHandler public void onBlockFromTo(BlockFromToEvent event) { event.setCancelled(true); }
+	@EventHandler public void onBlockBurnEvent(BlockBurnEvent event) { event.setCancelled(true); }
+	@EventHandler public void onBlockIgniteEvent(BlockIgniteEvent event) { event.setCancelled(true); }
+	@EventHandler public void onItemDropEvent(PlayerDropItemEvent event) { event.setCancelled(true); }
+    @EventHandler public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) { event.setCancelled(true); }
+    @EventHandler public void onPlayerBucketFill(PlayerBucketFillEvent event) { event.setCancelled(true); }
+    @EventHandler public void onBlockPlace(BlockPlaceEvent event) { event.setCancelled(true); }
+    @EventHandler public void onBlockBreak(BlockBreakEvent event) { event.setCancelled(true); }
 
-	@EventHandler
-	public void onLeavesDecay(LeavesDecayEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onBlockFade(BlockFadeEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onBlockGrow(BlockGrowEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onBlockFromTo(BlockFromToEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onBlockBurnEvent(BlockBurnEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onBlockIgniteEvent(BlockIgniteEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onItemDropEvent(PlayerDropItemEvent event) {
-		event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
+	@EventHandler public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		player.getInventory().clear();
 		player.setGameMode(GameMode.ADVENTURE);
 	}
 
-	@EventHandler
-	public void onPlayerDamage(EntityDamageByEntityEvent event) {
+	@EventHandler public void onPlayerDamage(EntityDamageByEntityEvent event) {
 		event.setCancelled(true);
 		event.setDamage(0);
 	}
