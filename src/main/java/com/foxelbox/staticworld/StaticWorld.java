0252b1d6eafc97f6385f8f21759a21a9c154fb70
Mark Dietzer <mriq91@gmail.com>
2014-06-01 16:28:11 +0200

    de.doridian -> com.foxelbox

--- src/main/java/com/foxelbox/staticworld/StaticWorld.java
+++ src/main/java/com/foxelbox/staticworld/StaticWorld.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with StaticWorld.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.doridian.staticworld;
+package com.foxelbox.staticworld;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
